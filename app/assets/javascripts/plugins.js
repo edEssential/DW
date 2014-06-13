acc61486d9791129737328df4f57bd5a54425e67
madebyeggs <madebyeggs@gmail.com>
2014-06-13 10:05:06 +0100

    updates following david's email

--- app/assets/javascripts/plugins.js
+++ app/assets/javascripts/plugins.js
@@ -580,11 +580,11 @@ $(document).ready(function() {
 
 			// Map Coordination
 
-			var latlng = new google.maps.LatLng(41.862274,-87.661328);
+			var latlng = new google.maps.LatLng(51.518423, -0.166051);
 
 			// Map Options
 			var myOptions = {
-				zoom: 15,
+				zoom: 10,
 				center: latlng,
 				mapTypeId: google.maps.MapTypeId.ROADMAP,
 				disableDefaultUI: true,
@@ -600,21 +600,16 @@ $(document).ready(function() {
 
 		  	// First Marker Coordination
 			
-			var myLatlng = new google.maps.LatLng(41.856774,-87.679928);
+			var myLatlng = new google.maps.LatLng(51.538754, -0.243416);
 
 			// Your Texts 
 
-			 var contentString = '<div id="content">'+
+			 var contentString = '<div id="mapsContent">'+
 			  '<div id="siteNotice">'+
 			  '</div>'+
-			  '<h4>' +
-
-			  'Office 1'+
-
-			  '</h4>'+
 			  '<p>' +
 
-			  'Your description is here.' +
+			  'You can find us at:' + '<br/>' + '39 Sellons Avenue, NW10 4HJ' + 
 
 			  '</p>'+
 			  '</div>';
@@ -623,7 +618,7 @@ $(document).ready(function() {
 			var marker = new google.maps.Marker({
 				  position: myLatlng,
 				  map: map,
-				  title: 'Hello World!',
+				  title: 'The Office',
 				  icon: image
 			  });
 
@@ -638,49 +633,6 @@ $(document).ready(function() {
 			  });
 
 			 /* ========= End First Marker ========= */
-
-
-
-
-			 /* ========= Second Marker ========= */
-
-			 // Second Marker Coordination
-
-			 var myLatlngSecond = new google.maps.LatLng(41.863774,-87.639928);
-
-			 // Your Texts
-
-			 var contentStringSecond = '<div id="content">'+
-			  '<div id="siteNotice">'+
-			  '</div>'+
-			  '<h4>' +
-
-			  'Office 2'+
-
-			  '</h4>'+
-			  '<p>' +
-
-			  'Your description is here.' +
-
-			  '</p>'+
-			  '</div>';
-
-			  var infowindowSecond = new google.maps.InfoWindow({
-				  content: contentStringSecond,
-				  });
-
-			 var markerSecond = new google.maps.Marker({
-				  position: myLatlngSecond,
-				  map: map,
-				  title: 'Hello World!',
-				  icon: image
-			  });
-
-			 google.maps.event.addListener(markerSecond, 'click', function() {
-				infowindowSecond.open(map,markerSecond);
-			  });
-
-			 /* ========= End Second Marker ========= */
 		
 		})
 
